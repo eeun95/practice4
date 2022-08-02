9426384670e2338322647241e7ddeeecd771b78c
eeun <eeun95@gmail.com>
2022-08-02 21:05:54 +0900

    AOP 프로그래밍 실습

--- src/main/java/chap07/ExeTimeCalculator.java
+++ src/main/java/chap07/ExeTimeCalculator.java
@@ -4,6 +4,7 @@ public class ExeTimeCalculator implements Calculator{
 
     private Calculator delegate;
 
+    // 객체를 생성자를 통해 받아와서
     public ExeTimeCalculator(Calculator delegate) {
         this.delegate = delegate;
     }
@@ -11,6 +12,7 @@ public class ExeTimeCalculator implements Calculator{
     @Override
     public long factorial(long num) {
         long start = System.nanoTime();
+        // 핵심 기능은 다른 객체에 위임함
         long result = delegate.factorial(num);
         long end = System.nanoTime();
         System.out.printf("%s.factorial(%d) 실행 시간 = %d\n",
